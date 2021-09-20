42856d641059c0344dfef652dfcbecc97e6088af
sutthie <38927568+sutthie@users.noreply.github.com>
2021-09-20 15:45:45 +0700

    Update Profile.js

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -27,21 +27,23 @@ const Profile = ({ login }) => {
   // const match = useRouteMatch();
   // console.log(match.url + ' : ' + match.path);
 
+  // alt + 96 `
+
   return (
     <div className="container">
       <h2 className="text-center text-primary">Profile Page</h2>
       <ul>
         <li>
-          <Link to={'${url}/viewprofile'}>View Profile</Link>
+          <Link to={`${url}/viewprofile`}>View Profile</Link>
         </li>
         <li>
-          <Link to={'${url}/editprofile'}>Edit Profile</Link>
+          <Link to={`${url}/editprofile`}>Edit Profile</Link>
         </li>
       </ul>
 
       <Switch>
-        <Route path={'${path}/viewprofile'} component={ViewProfile} />
-        <Route path={'${path}/editprofile'} component={EditProfile} />
+        <Route path={`${path}/viewprofile`} component={ViewProfile} />
+        <Route path={`${path}/editprofile`} component={EditProfile} />
       </Switch>
     </div>
   );
